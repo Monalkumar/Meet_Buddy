34a320fd4643778287121e8151f246e3c54202bc
MonalKumar <monalshiv123@gmail.com>
2025-06-27 02:27:31 +0530

    authbasics

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ const express = require("express");
 const app = express();
 
 
-app.use((req,res)=>{
-    console.log("hello response succesfully")
-    res.send("hello response 1")
-})
-
+app.get("/admin/getAllData",(req,res)=>{
+    const token = "abc";
+    const isAdminAuthorized = token ==="abc"
+  if(isAdminAuthorized){
+    res.send("Admin is authorised")
+  } 
+  else{
+    res.status(401).send("please check the admin authority");
+  }
+  })
+  
 app.listen(5000,()=>{
     console.log("server is running successfuly in the port for node js")
 })
+
+
+
+
+    
